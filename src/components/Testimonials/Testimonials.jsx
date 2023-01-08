5b03a39026d10a65b08d7ac7186735791a50e10f
cyrusnderitu <cyrusnderitu.k@gmail.com>
2023-01-08 22:14:22 +0300

    Form section, Property and testimonials code

---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <div className="my-20 bg-orange-100 h-[300px]">
+      <div className="px-24 flex flex-1 gap-x-8">
+        <div className="left flex-1">
+            <div className="cont">
+                <p className="">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit
+                    ullam molestias reiciendis quis voluptate nam possimus expedita est
+                    iusto voluptates officiis, velit aliquam veniam iste cupiditate amet
+                    autem sequi quo! Tempora placeat cumque voluptatibus iusto sapiente
+                    nemo repudiandae possimus, reprehenderit minima ex praesentium, quia
+                    hic.
+                </p>
+                <div className="user">
+                    
+                </div>
+            </div>
+        </div>
+        <div className="right flex-1">
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
